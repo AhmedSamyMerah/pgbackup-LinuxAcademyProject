04f9751cda3abd3f0d8423b044a60d45905a09b8
Ahmed Samy Merah <meraha@sheridancollege.ca>
2020-06-13 06:06:47 +0000

    Various modifications to output and use

--- setup.py
+++ setup.py
@@ -11,6 +11,11 @@ setup(
     description = 'A utility for backing up PostgreSQL databases',
     long_description = long_description,
     long_description_content_type = 'text/markdown',
-    url = 'https://github.com/AhmedSamyMerah'
-    packages = find_packages('src')
+    url = 'https://github.com/AhmedSamyMerah',
+    packages = find_packages('src'),
+    package_dir ={'':'src'},
+    install_requires=['boto3'],
+    entry_points={
+        'console_scripts' : ['pgbackup=pgbackup.cli:main',],
+    }
 )
